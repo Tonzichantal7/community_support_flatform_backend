9ccc9a9b9db5ff395f318ad3cb62a85aca929243
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2026-02-18 02:42:42 -0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,9 +25,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:3000',
+      // 'http://localhost:5173',
+      // 'http://localhost:5174',
+      // 'http://localhost:3000',
       'https://community-support-service-exchange.vercel.app'
     ],
     credentials: true,
